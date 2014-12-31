1ffe7fc43783305424979a928d6393775ba258f0
James Gibson Weber <j.gibweb@gmail.com>
2014-12-31 15:19:31 -0800

    setting up mongoDB for game instances

--- app.js
+++ app.js
@@ -10,9 +10,19 @@ app.use(multer({ dest: './uploads/'}))
 var env = process.env.NODE_ENV || 'dev';
 
 
-
-// DB SETUP
-
+var mongoose = require("mongoose");
+if (env === 'dev') {
+  
+}
+require('./db');
+// MongoDB config
+mongoose.connect(process.env.MONGO_CREDS, function(err, res) {
+  if(err) {
+    console.log('error connecting to MongoDB Database. ' + err);
+  } else {
+    console.log('Connected to Database');
+  }
+});
 
 
 app.set('view engine', 'ejs');
@@ -29,5 +39,5 @@ app.get('/', function(req, res) {
 
 var port = process.env.PORT || 3000;
 app.listen(port, function() {
-  console.log('shh, im working over here...')
+  console.log('preparing the slidezies...')
 });
